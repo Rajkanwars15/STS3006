5e9bf6a342c153c3e6fdc1b4d094d30ebcafc95a
Rajkanwars15 <rajkanwars15@outlook.com>
2023-07-12 17:43:30 +0530

    update

--- src/Fibonacci.java
+++ src/Fibonacci.java
@@ -1,16 +1,15 @@
-import  java.util.Scanner;
-class Fibonacci {
+public class Fibonacci {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int a = 0, b = 1;
-        int count = 0;
-        while(count < n){
-            System.out.print(a+" ");
-            int c = a + b;
-            a = b;
-            b = c;
-            count++;
+        int rows = 5;
+
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= rows - i; j++) {
+                System.out.print("*  ");
+            }
+            for (int k = 1; k <= i; k++) {
+                System.out.print(" ");
+            }
+            System.out.println();
         }
     }
 }
